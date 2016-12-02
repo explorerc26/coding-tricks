6a983cf7f00d0396e54ac8afed51416c29e6dccb
explorerc26 <svmc26@gmail.com>
2016-12-02 10:27:40 -0500

    initial

---
+++ coding-tricks/src/main/java/com/explorer/core/Basics.java
@@ -0,0 +1,16 @@
+package com.explorer.core;
+
+public class Basics {
+	public static void main(String[] args) {
+		// Single quotes vs double quotes in java
+		charactersVsString();
+	}
+	
+	public static void charactersVsString(){
+		 System.out.println("H" + "a");
+		 System.out.println('H' + 'a');
+		 //outouts 
+		 //Ha
+		 //169
+	}
+}
