751d0d8e776af738723c59ce60b0caa9df8e4541
smunigala <m.srinivaas@gmail.com>
2017-01-02 18:42:40 -0500

    .

--- coding-tricks/src/main/java/com/explorer/core/FinallyDemo.java
+++ coding-tricks/src/main/java/com/explorer/core/FinallyDemo.java
@@ -3,6 +3,14 @@ package com.explorer.core;
 public class FinallyDemo {
 
 	public static void main(String[] args) {
+		
+//		finally block get executed if we insert a return statement	inside the try block of a try-catch-finally
+		
+//		Note that there are some cases in which the finally block will not get executed, such as the following:
+//		• If the virtual machine exits during try / catch block execution.
+//		• If the thread which is executing during the try / catch block gets killed.
+
+		
 		System.out.println("going to try catch block");
 		try{
 			System.out.println("I am in try block");
