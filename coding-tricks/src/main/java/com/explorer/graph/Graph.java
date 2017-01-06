c24690ddbff6a8a53ca07adf1dfa0d0a2e9c2499
explorerc26 <svmc26@gmail.com>
2017-01-06 16:53:46 -0500

    .
    .

---
+++ coding-tricks/src/main/java/com/explorer/graph/Graph.java
@@ -0,0 +1,5 @@
+package com.explorer.graph;
+
+public class Graph {
+	private int vertices;
+}
---
+++ coding-tricks/src/main/java/com/explorer/search/GraphUtil.java
@@ -0,0 +1,7 @@
+package com.explorer.search;
+
+public class GraphUtil {
+	public static void createSampleGraph(){
+		
+	}
+}
