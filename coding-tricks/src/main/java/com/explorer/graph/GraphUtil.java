b86e63b2259a0cc27149c24cf1275aca92a9cdec
explorerc26 <svmc26@gmail.com>
2017-01-09 17:03:59 -0500

    .
    .

--- coding-tricks/src/main/java/com/explorer/graph/GraphUtil.java
+++ coding-tricks/src/main/java/com/explorer/graph/GraphUtil.java
@@ -2,13 +2,27 @@ package com.explorer.graph;
 
 public class GraphUtil {
 	public static Graph createSampleGraph(){
-		Graph g = new Graph(4);
-        g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(1, 2);
-        g.addEdge(2, 0);
+//		Graph g = new Graph(4);
+//        g.addEdge(0, 1);
+//        g.addEdge(0, 2);
+//        g.addEdge(1, 2);
+//        g.addEdge(2, 0);
+//        g.addEdge(2, 3);
+//        g.addEdge(3, 3);
+//        return g;
+		
+		Graph g = new Graph(8);
+        g.addEdge(4, 2);
+        g.addEdge(4, 1);
+        g.addEdge(2, 1);
         g.addEdge(2, 3);
-        g.addEdge(3, 3);
+        g.addEdge(2, 6);
+        g.addEdge(2, 5);
+        g.addEdge(5, 7);
+        g.addEdge(6, 7);
+        g.addEdge(3, 6);
+        g.addEdge(1, 3);
+       
         return g;
 	}
 }
--- coding-tricks/src/main/java/com/explorer/search/DepthFirstSearch.java
+++ coding-tricks/src/main/java/com/explorer/search/DepthFirstSearch.java
@@ -1,5 +1,38 @@
 package com.explorer.search;
 
-public class DepthFirstSearch {
+import java.util.Iterator;
+import java.util.Stack;
 
+import com.explorer.graph.Graph;
+import com.explorer.graph.GraphUtil;
+
+public class DepthFirstSearch {
+	
+	public static void main(String[] args) {
+		Graph graph = GraphUtil.createSampleGraph();
+		new DepthFirstSearch().traverse(graph, 4);
+	}
+
+	public void traverse(Graph g, int node) {
+        // set as false)
+        boolean[] visited = new boolean[g.getTotalVertices()];
+ 
+        // Create a stack for DFS
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.add(node);
+		visited[node] = true;
+		while(!stack.isEmpty()){
+			int currentNode=stack.pop();  
+			System.out.print(currentNode + "\t"); 
+        	Iterator<Integer> iterator = g.adjacencyLists[currentNode].listIterator();
+        	while(iterator.hasNext()){
+        		int n = iterator.next();
+        		if(visited[n]== false){
+        			visited[n]= true;
+        			stack.add(n);
+        		}
+        	}
+		}
+		
+	}
 }
