4588f2a39039b125ca0d6579d36faf7a0ee9be53
smunigala <m.srinivaas@gmail.com>
2017-01-21 21:51:37 -0500

    .
    .

---
+++ coding-tricks/src/main/java/com/explorer/core/Example.java
@@ -0,0 +1,11 @@
+package com.explorer.core;
+
+public class Example {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		//no issues below :) 
+		http://helloworld.com
+		System.exit(0);
+	}
+}
