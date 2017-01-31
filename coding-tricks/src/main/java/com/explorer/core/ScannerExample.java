22df42fe955a8b6316f99c95341ff59f24bb4edc
explorerc26 <svmc26@gmail.com>
2017-01-31 14:11:04 -0500

    .
    .

---
+++ coding-tricks/src/main/java/com/explorer/core/ScannerExample.java
@@ -0,0 +1,34 @@
+package com.explorer.core;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ScannerExample {
+	public static void main(String[] args) {
+		/*
+		 * Enter your code here. Read input from STDIN. Print output to STDOUT.
+		 * Your class should be named Solution.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int no = Integer.parseInt(sc.nextLine());
+		Map<String, Integer> words = new HashMap<String, Integer>();
+		for (int i = 0; i < no; i++) {
+			String word = sc.nextLine();
+			if (words.containsKey(word)) {
+				words.put(word, words.get(word) + 1);
+			} else {
+				words.put(word, 1);
+			}
+		}
+		int queryNo = Integer.parseInt(sc.nextLine());
+		for (int i = 0; i < queryNo; i++) {
+			String query = sc.nextLine();
+			if (words.get(query) == null) {
+				System.out.println(0);
+			} else {
+				System.out.println(words.get(query));
+			}
+		}
+	}
+}
