1cf92da4ca99644b6afcf4dbed736311737c8570
explorerc26 <svmc26@gmail.com>
2017-02-11 23:40:07 -0500

    .
    .

---
+++ coding-tricks/src/main/java/com/explorer/core/tricks/JavaPuzzler3.java
@@ -0,0 +1,13 @@
+package com.explorer.core.tricks;
+
+public class JavaPuzzler3 {
+	public static void main(String[] args) {
+		String[] arr = { "java ", "puzzlers ", "is ", "a ", "good ", "book" };
+		String message = null;
+		for (String str : arr) {
+			message += str;
+		}
+		System.out.println(message);
+	}
+
+}
