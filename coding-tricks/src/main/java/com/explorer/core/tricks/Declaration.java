79774bd50cdbc4eb90ce473461dc8fcf4c9ce7e0
smunigala <m.srinivaas@gmail.com>
2017-02-16 20:49:18 -0500

    .
    .

---
+++ coding-tricks/src/main/java/com/explorer/core/tricks/Declaration.java
@@ -0,0 +1,16 @@
+package com.explorer.core.tricks;
+
+public class Declaration {
+
+	public static void main(String[] args) {
+		 //here matric is 2d array
+		 int[] rowvector, colvector, matrix[];
+		 matrix = new int[10][10];
+		 byte rowvector1[], colvector1[], matrix1[][];
+		 //all below are arrays
+		 int[] a, b, c;
+		 // only d is array
+		 int d[], e, f;
+	}
+
+}
